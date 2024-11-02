c3ea46498f1ed76b6a302c7a0162c5a5d67fcae6
MuniS97 <tursunovmunis141@gmail.com>
2024-11-02 22:54:10 +0500

    fixed

--- components/custom/SaleProduct.tsx
+++ components/custom/SaleProduct.tsx
@@ -13,23 +13,36 @@ interface Props {
         discount: number;
         image: string;
         button: string;
-    }
+    };
 }
 
 export const SaleProduct: React.FC<Props> = ({ className, translation }) => {
     return (
-        <div className={cn("rounded-3xl w-full h-full bg-[url('/images/saleBg.webp')] bg-cover bg-center bg-no-repeat]", className)}>
-            <div className='rounded-3xl w-full h-full bg-csemigray flex flex-col sm:flex-row justify-between items-center gap-5 p-10 md:p-5 lg:p-10'>
-                <Image src={translation?.image} width={200} height={200} alt={`Perfume ${translation?.id}`} className='w-[50%] lg:w-full lg:h-full object-cover' />
-                <div className='flex flex-col justify-center items-center sm:items-start sm:text-left gap-2'>
-                    <h4 className='text-white text-[2em] font-bold'>{translation?.title}</h4>
-                    <span className='uppercase text-[4em] md:text-[3em] lg:text-[5em] font-bold text-transparent leading-[50px]' style={{ WebkitTextStroke: '1px #f2c437' }}>{translation?.subtitle}</span>
-                    <p className='text-white text-[3em] font-bold'>{translation?.discount}%</p>
+        <div className={cn("rounded-3xl w-full h-full bg-[url('/images/saleBg.webp')] bg-cover bg-center bg-no-repeat", className)}>
+            <div className="rounded-3xl w-full h-full bg-csemigray flex flex-col lg:flex-row justify-between items-center gap-5 p-6 md:p-8 lg:p-10">
+                <div className="w-full lg:w-1/2 flex justify-center items-center">
+                    <Image
+                        src={translation?.image}
+                        width={200}
+                        height={200}
+                        alt={`Perfume ${translation?.id}`}
+                        className="w-full max-w-[150px] sm:max-w-[200px] lg:max-w-[250px] object-cover"
+                    />
+                </div>
+                <div className="w-full lg:w-1/2 flex flex-col justify-center items-center lg:items-start gap-2 text-center lg:text-left">
+                    <h4 className="text-white text-2xl md:text-3xl font-bold">{translation?.title}</h4>
+                    <span
+                        className="uppercase text-3xl md:text-4xl lg:text-5xl font-bold text-transparent leading-[1] md:leading-[1.2] lg:leading-[1.2]"
+                        style={{ WebkitTextStroke: '1px #f2c437' }}
+                    >
+                        {translation?.subtitle}
+                    </span>
+                    <p className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">{translation?.discount}%</p>
                     <Button
                         variant={"link"}
                         className="text-cgold hover:no-underline flex justify-center items-center hover:scale-105 hover:text-cgold/80 transition duration-200 p-0"
                     >
-                        Learn More <MoveRight width={24} height={24} />
+                        {translation?.button} <MoveRight width={24} height={24} />
                     </Button>
                 </div>
             </div>
